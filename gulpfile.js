48abc24510578044d494bf8ece1302be6cfabba0
suits-at <office@suits.at>
2016-05-02 12:22:32 +0200

    created src files created gulp tasks created index.html added gulp-rename and gulp-concat added directories in dist to .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,43 @@
+// Include gulp
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-    // place code for your default task here
+// Include Our Plugins
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
 });
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Minify images
+gulp.task('images', function () {
+    gulp.src('src/images/*.{png,gif,jpg}')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/scss/*.scss', ['sass']);
+    gulp.watch('src/js/*.js', ['scripts']);
+    gulp.watch('src/images/*.{png,gif,jpg}', ['images']);
+});
+
+// Default Task
+gulp.task('default', ['sass', 'scripts', 'images', 'watch']);
---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+console.log('ich bin auch noch da!');
