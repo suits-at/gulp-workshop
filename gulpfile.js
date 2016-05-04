f4fed95df8de4ae90b5e9c62733c14e5ceedae01
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-05-04 14:24:44 +0200

    Code review with slimmer code style

--- gulpfile.js
+++ gulpfile.js
@@ -10,33 +10,33 @@ var imagemin = require('gulp-imagemin');
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('src/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
+  return gulp.src('src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist/js'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+  return gulp.src('src/js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
 });
 
 // Minify images
 gulp.task('images', function () {
-    return gulp.src('src/images/*.{png,gif,jpg}')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'));
+  return gulp.src('src/images/*.{png,gif,jpg}')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('src/scss/*.scss', ['sass']);
-    gulp.watch('src/js/*.js', ['scripts']);
-    gulp.watch('src/images/*.{png,gif,jpg}', ['images']);
+  gulp.watch('src/scss/*.scss', ['sass']);
+  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('src/images/*.{png,gif,jpg}', ['images']);
 });
 
 // Default Task
